d54bf26fc895cfab1ec02478a4d918c6f4a190fd
ST10069127 <st10069127@vcconnect.edu.za>
2025-02-04 23:18:32 +0200

    Initial commit - Uploading MVC Application

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+using System.Diagnostics;
+using KhumalocraftEmporium_ST10069127_CLDV6211POE.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KhumalocraftEmporium_ST10069127_CLDV6211POE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult MyWork()
+        {
+            return View();
+        }
+
+        public IActionResult CallCenter()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KhumalocraftEmporium_ST10069127_CLDV6211POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
